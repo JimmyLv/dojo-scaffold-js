ceda2caa84d975d5482a8893a8ec2f7ad44d0c64
JimmyLv <jimmy.jinglv@gmail.com>
2019-09-02 16:14:32 +0800

    fix: simplify if-else

--- src/dojo/gildedrose/GildedRose.js
+++ src/dojo/gildedrose/GildedRose.js
@@ -2,67 +2,56 @@ export default class GildedRose {
   constructor(items) {
     this.items = items
   }
+  isAgedBrie(item) {
+    return item.name === 'Aged Brie'
+  }
 
+  isBackstagePass(item) {
+    return item.name === 'Backstage passes to a TAFKAL80ETC concert'
+  }
+
+  isSulfuras(item) {
+    return item.name === 'Sulfuras, Hand of Ragnaros'
+  }
+
+  isBetterQualityWhenTimeGoesBy(item) {
+    return this.isAgedBrie(item) || this.isBackstagePass(item)
+  }
   updateQuality() {
-    function isAgedBrie(item) {
-      return item.name === 'Aged Brie'
-    }
-
-    function isBackstagePass(item) {
-      return item.name === 'Backstage passes to a TAFKAL80ETC concert'
-    }
-
-    function isSulfuras(item) {
-      return item.name === 'Sulfuras, Hand of Ragnaros'
-    }
-
     this.items.forEach(item => {
-      if (!(isAgedBrie(item))
-        && !(isBackstagePass(item))) {
-        if (item.quality > 0) {
-          if (!(isSulfuras(item))) {
-            item.quality = item.quality - 1
-          }
+      if (!this.isBetterQualityWhenTimeGoesBy(item)) {
+        if (item.quality > 0 && !this.isSulfuras(item)) {
+          item.quality = item.quality - 1
         }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
+      } else if (item.quality < 50) {
+        item.quality = item.quality + 1
 
-          if (isBackstagePass(item)) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
+        if (this.isBackstagePass(item)) {
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality = item.quality + 1
+          }
 
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
+          if (item.sellIn < 6 && item.quality < 50) {
+            item.quality = item.quality + 1
           }
         }
       }
 
-      if (!isSulfuras(item)) {
+      if (!this.isSulfuras(item)) {
         item.sellIn = item.sellIn - 1
       }
 
       if (item.sellIn < 0) {
-        if (!isAgedBrie(item)) {
-          if (!isBackstagePass(item)) {
-            if (item.quality > 0) {
-              if (!isSulfuras(item)) {
-                item.quality = item.quality - 1
-              }
+        if (!this.isAgedBrie(item)) {
+          if (!this.isBackstagePass(item)) {
+            if (item.quality > 0 && !this.isSulfuras(item)) {
+              item.quality = item.quality - 1
             }
           } else {
             item.quality = 0
           }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
+        } else if (item.quality < 50) {
+          item.quality = item.quality + 1
         }
       }
     })
