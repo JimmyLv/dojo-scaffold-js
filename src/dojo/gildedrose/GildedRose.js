8f22b6ee012734681f4c0dac2ec855266eea6bc1
JimmyLv <jimmy.jinglv@gmail.com>
2019-09-02 16:19:24 +0800

    fix: move name related logic into item inside

--- src/dojo/gildedrose/GildedRose.js
+++ src/dojo/gildedrose/GildedRose.js
@@ -2,31 +2,16 @@ export default class GildedRose {
   constructor(items) {
     this.items = items
   }
-  isAgedBrie(item) {
-    return item.name === 'Aged Brie'
-  }
-
-  isBackstagePass(item) {
-    return item.name === 'Backstage passes to a TAFKAL80ETC concert'
-  }
-
-  isSulfuras(item) {
-    return item.name === 'Sulfuras, Hand of Ragnaros'
-  }
-
-  isBetterQualityWhenTimeGoesBy(item) {
-    return this.isAgedBrie(item) || this.isBackstagePass(item)
-  }
   updateQuality() {
     this.items.forEach(item => {
-      if (!this.isBetterQualityWhenTimeGoesBy(item)) {
-        if (item.quality > 0 && !this.isSulfuras(item)) {
+      if (!item.isBetterQualityWhenTimeGoesBy()) {
+        if (item.quality > 0 && !item.isSulfuras()) {
           item.quality = item.quality - 1
         }
       } else if (item.quality < 50) {
         item.quality = item.quality + 1
 
-        if (this.isBackstagePass(item)) {
+        if (item.isBackstagePass()) {
           if (item.sellIn < 11 && item.quality < 50) {
             item.quality = item.quality + 1
           }
@@ -37,14 +22,14 @@ export default class GildedRose {
         }
       }
 
-      if (!this.isSulfuras(item)) {
+      if (!item.isSulfuras()) {
         item.sellIn = item.sellIn - 1
       }
 
       if (item.sellIn < 0) {
-        if (!this.isAgedBrie(item)) {
-          if (!this.isBackstagePass(item)) {
-            if (item.quality > 0 && !this.isSulfuras(item)) {
+        if (!item.isAgedBrie()) {
+          if (!item.isBackstagePass()) {
+            if (item.quality > 0 && !item.isSulfuras()) {
               item.quality = item.quality - 1
             }
           } else {
--- src/dojo/gildedrose/Item.js
+++ src/dojo/gildedrose/Item.js
@@ -5,6 +5,22 @@ export default class Item {
     this.quality = quality
   }
 
+  isAgedBrie() {
+    return this.name === 'Aged Brie'
+  }
+
+  isBackstagePass() {
+    return this.name === 'Backstage passes to a TAFKAL80ETC concert'
+  }
+
+  isSulfuras() {
+    return this.name === 'Sulfuras, Hand of Ragnaros'
+  }
+
+  isBetterQualityWhenTimeGoesBy() {
+    return this.isAgedBrie() || this.isBackstagePass()
+  }
+
   toString() {
     return this.name + ', ' + this.sellIn + ', ' + this.quality + '\n'
   }
