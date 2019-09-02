9b620856ca8d4c8406e6c666cd3a44b1c2946531
JimmyLv <jimmy.jinglv@gmail.com>
2019-09-02 16:43:54 +0800

    refactor: extract update methods

--- src/dojo/gildedrose/Item.js
+++ src/dojo/gildedrose/Item.js
@@ -22,6 +22,15 @@ export default class Item {
   }
 
   updateQualityAndSellInDays() {
+    this.updateSellInDays()
+    this.updateQuality()
+
+    if (this.isExpired()) {
+      this.updateQualityAfterExpiration()
+    }
+  }
+
+  updateQuality() {
     if (!this.isBetterQualityWhenTimeGoesBy()) {
       if (this.quality > 0 && !this.isSulfuras()) {
         this.quality = this.quality - 1
@@ -39,23 +48,29 @@ export default class Item {
         }
       }
     }
+  }
 
+  updateQualityAfterExpiration() {
+    if (!this.isAgedBrie()) {
+      if (!this.isBackstagePass()) {
+        if (this.quality > 0 && !this.isSulfuras()) {
+          this.quality = this.quality - 1
+        }
+      } else {
+        this.quality = 0
+      }
+    } else if (this.quality < 50) {
+      this.quality = this.quality + 1
+    }
+  }
+
+  isExpired() {
+    return this.sellIn < 0
+  }
+
+  updateSellInDays() {
     if (!this.isSulfuras()) {
       this.sellIn = this.sellIn - 1
-    }
-
-    if (this.sellIn < 0) {
-      if (!this.isAgedBrie()) {
-        if (!this.isBackstagePass()) {
-          if (this.quality > 0 && !this.isSulfuras()) {
-            this.quality = this.quality - 1
-          }
-        } else {
-          this.quality = 0
-        }
-      } else if (this.quality < 50) {
-        this.quality = this.quality + 1
-      }
     }
   }
 
