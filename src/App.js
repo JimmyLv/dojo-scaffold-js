5835fdb9253ee675bf16220c40382638ec9198e9
JimmyLv <jimmy.jinglv@gmail.com>
2019-09-06 13:55:48 +0800

    feat: add AddTodo component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import TodoItem from './components/TodoItem'
 import FilterInfo from './components/FilterInfo'
+import AddTodo from './components/AddTodo'
 
 function App() {
   return (
     <div>
       <h1>Todo</h1>
-      <FilterInfo filters={['All', 'Done']}/>
+      <FilterInfo filters={['All', 'Done']} />
+      <AddTodo />
       {[].forEach(item => <TodoItem {...item} />)}
     </div>
   )
---
+++ src/components/AddTodo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddTodo() {
+  return <><input type="text" placeholder={'Do that thing...'} />
+    <button>+</button>
+  </>
+}
