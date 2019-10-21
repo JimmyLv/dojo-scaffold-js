c02ab903d42b6c03161cf4191fe97dc73ed65f7d
JimmyLv <jimmy.jinglv@gmail.com>
2019-10-21 17:45:22 +0800

    feat: just verify the fetch on test

--- src/store/todos.test.js
+++ src/store/todos.test.js
@@ -1,25 +1,32 @@
 import reducer, { actions } from './todos'
 
 describe('todos reducers', () => {
+  it.skip('should fetch all todos from api', () => {
+    fetch.mockResponse(
+      JSON.stringify([{ id: 0, text: 'new todo.', completed: false }])
+    )
+
+    const result = reducer([], actions.fetchTodos())
+
+    expect(result).toBe([{ id: 0, text: 'new todo.', completed: false }])
+  })
   it('should add todo in store state', () => {
     const result = reducer([], actions.addTodo('new todo.'))
 
-    expect(result).toEqual([
-      { id: 0, text: 'new todo.', completed: false },
-    ])
+    expect(result).toEqual([{ id: 3, text: 'new todo.', completed: false }])
   })
   it('should toggle todo status in store state', () => {
-    const state = [{
-      id: 1,
-      text: 'new todo.',
-      completed: false,
-    }]
+    const state = [
+      {
+        id: 1,
+        text: 'new todo.',
+        completed: false,
+      },
+    ]
 
     const result = reducer(state, actions.toggle(1))
 
-    expect(result).toEqual([
-      { id: 1, text: 'new todo.', completed: true },
-    ])
+    expect(result).toEqual([{ id: 1, text: 'new todo.', completed: true }])
   })
   it('should remove todo in store state', () => {
     const state = [
@@ -27,7 +34,8 @@ describe('todos reducers', () => {
         id: 1,
         text: 'new todo.',
         completed: true,
-      }, {
+      },
+      {
         id: 2,
         text: 'another new todo.',
         completed: false,
@@ -36,8 +44,6 @@ describe('todos reducers', () => {
 
     const result = reducer(state, actions.remove(2))
 
-    expect(result).toEqual([
-      { id: 1, text: 'new todo.', completed: true },
-    ])
+    expect(result).toEqual([{ id: 1, text: 'new todo.', completed: true }])
   })
 })
