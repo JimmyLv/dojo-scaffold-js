d2784518af4b3874e8181c821fa956026bd222ab
JimmyLv <jimmy.jinglv@gmail.com>
2019-10-21 15:33:34 +0800

    feat: add CURD tests for api

--- cypress/integration/apis/todos.js
+++ cypress/integration/apis/todos.js
@@ -12,6 +12,17 @@ describe('todos API', () => {
 
   const getItems = () => cy.request('/api/todos').its('body')
 
+  const addItem = item => cy.request('POST', '/api/todos', item)
+
+  const deleteItem = item => cy.request('DELETE', `/api/todos/${item.id}`)
+
+  const deleteAll = () => getItems().each(deleteItem)
+
+  const reset = () => {
+    deleteAll()
+    initialItems.forEach(addItem)
+  }
+
   it('returns JSON', () => {
     cy.request('/api/todos')
       .its('headers')
@@ -29,6 +40,27 @@ describe('todos API', () => {
 
     it('returns id + task objects', () => {
       getItems().each(value => expect(value).to.have.all.keys('id', 'task'))
+    })
+  })
+
+  describe('CURD tests', () => {
+    beforeEach(reset)
+    afterEach(reset)
+
+    it('add an item', () => {
+      const randomId = Cypress._.random(0, 10000)
+      const item = { id: randomId, task: 'life hacker' }
+
+      addItem(item)
+      cy.request(`/api/todos/${randomId}`)
+        .its('body')
+        .should('deep.eq', item)
+    })
+
+    it('delete an item', () => {
+      const id = initialItems[0].id
+      cy.request('DELETE', `/api/todos/${id}`)
+      getItems().should('have.length', 1)
     })
   })
 })
