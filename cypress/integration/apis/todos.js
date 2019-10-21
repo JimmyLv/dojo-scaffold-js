fd95f771c97a65c216cc0606d7f66e48fa6bf022
JimmyLv <jimmy.jinglv@gmail.com>
2019-10-21 15:17:12 +0800

    feat: add /api as prefix

--- cypress/integration/apis/todos.js
+++ cypress/integration/apis/todos.js
@@ -1,8 +1,30 @@
 describe('todos API', () => {
+  const initialItems = [
+    {
+      "id": 1,
+      "task": "read something"
+    },
+    {
+      "id": 2,
+      "task": "write something"
+    }
+  ]
+
+  const getItems = () =>
+    cy.request('/todos')
+      .its('body')
+
   it('returns JSON', () => {
-    cy.request('http://localhost:8080/todos')
+    cy.request('/api/todos')
       .its('headers')
       .its('content-type')
       .should('include', 'application/json')
   })
+  it('loads 2 items', () => {
+    cy.request('/api/todos')
+      .its('body')
+      .should('have.length', 2)
+  })
+
+
 })
