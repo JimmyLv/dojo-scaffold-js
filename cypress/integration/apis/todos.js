755ff59deb6612254c0f9b3cada6c5afc8b47bda
JimmyLv <jimmy.jinglv@gmail.com>
2019-10-21 15:24:43 +0800

    feat: add idempotent tests for api

--- cypress/integration/apis/todos.js
+++ cypress/integration/apis/todos.js
@@ -1,18 +1,16 @@
 describe('todos API', () => {
   const initialItems = [
     {
-      "id": 1,
-      "task": "read something"
+      id: 1,
+      task: 'read something',
     },
     {
-      "id": 2,
-      "task": "write something"
-    }
+      id: 2,
+      task: 'write something',
+    },
   ]
 
-  const getItems = () =>
-    cy.request('/todos')
-      .its('body')
+  const getItems = () => cy.request('/api/todos').its('body')
 
   it('returns JSON', () => {
     cy.request('/api/todos')
@@ -21,10 +19,16 @@ describe('todos API', () => {
       .should('include', 'application/json')
   })
   it('loads 2 items', () => {
-    cy.request('/api/todos')
-      .its('body')
-      .should('have.length', 2)
+    getItems().should('have.length', 2)
   })
 
+  describe('Idempotent tests', () => {
+    it('loads the initial items', () => {
+      getItems().should('deep.eq', initialItems)
+    })
 
+    it('returns id + task objects', () => {
+      getItems().each(value => expect(value).to.have.all.keys('id', 'task'))
+    })
+  })
 })
