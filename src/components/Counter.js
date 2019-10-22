5e1044c2e2c6bd841602da9f548e7ff0e6badfc1
JimmyLv <jimmy.jinglv@gmail.com>
2019-10-22 23:18:14 +0800

    feat: added images screenshots and update the space between btns

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 export default class Counter extends Component {
   render() {
     const { product, onDecrease, onIncrease } = this.props
     return (
-      <div>
+      <Group>
         <button data-testid="decreaseCount" onClick={() => onDecrease(product)}>
           -
         </button>
@@ -12,7 +13,14 @@ export default class Counter extends Component {
         <button data-testid="increaseCount" onClick={() => onIncrease(product)}>
           +
         </button>
-      </div>
+      </Group>
     )
   }
 }
+
+const Group = styled.div`
+  display: flex;
+  align-items: center;
+  min-width: 80px;
+  justify-content: space-between;
+`
